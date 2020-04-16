3b53abc3af939040d97200ab613f0ce097955488
Artur-ershov <Artur-ershov@ya.ru>
2020-04-16 23:44:52 +0300

    added notion

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ app = Flask(__name__)
 def get(self):
   name = self.request.get('name')
   email = self.request.get('email')
-  print (name,email)
+
+client = NotionClient(token_v2="be078cd582e8b598b5a43aa13e4e7940fecc9a6915d56dedaabc1296bc7d6296e772a026be637d5dbeea4787f6c564cdcc71c8583155b5db7eac665c447747e78ed6f9c6f173216c06c74ce6fc61")
+page = client.get_collection_view("https://www.notion.so/yorsh/5309889ddccd405ea7b19472c1bf40fe?v=78cb1efa08064affae899b699703324f")
+
+row = page.collection.add_row()
+row.имя = name
+row.почта = email
+row.статус = "лид"
 
 
 if __name__ == '__main__':
@@ -20,9 +27,3 @@ if __name__ == '__main__':
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python37_app]
-
-client = NotionClient(token_v2="be078cd582e8b598b5a43aa13e4e7940fecc9a6915d56dedaabc1296bc7d6296e772a026be637d5dbeea4787f6c564cdcc71c8583155b5db7eac665c447747e78ed6f9c6f173216c06c74ce6fc61")
-
-page = client.get_block("https://www.notion.so/yorsh/5309889ddccd405ea7b19472c1bf40fe?v=78cb1efa08064affae899b699703324f")
-
-
